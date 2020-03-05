8599c837ff219c26c766dbe52dea2618c4df2ad9
EntryPointKR <entrypointkr@gmail.com>
2020-03-05 10:50:09 +0900

    Create libs directory on download

--- modules/legacy/legacy.gradle.kts
+++ modules/legacy/legacy.gradle.kts
@@ -13,7 +13,9 @@ dependencies {
 tasks {
     val downloadTask by registering(Download::class) {
         src("https://cdn.getbukkit.org/spigot/spigot-1.5.2-R1.1-SNAPSHOT.jar")
-        dest(file("libs"))
+        dest(file("libs").apply {
+            mkdirs()
+        })
         overwrite(false)
     }
     compileJava {
